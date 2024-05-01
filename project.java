1c1d49f071cac73f238af2ab33fccefd4396c012
chengruchou <chengruchou@gmail.com>
2024-05-01 10:35:46 +0800

    add index.html

--- project.java
+++ project.java
@@ -1,5 +1,6 @@
 public class project{
     public static void main(String args[]){
         System.out.println("Hello world!");
+        System.out.println("this is our project's corpse");
     }
 }
